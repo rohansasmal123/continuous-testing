de42dc6c96f33d16a496d1e1421e98dbee600e34
Rohan Sasmal <36567622+rohansasmal123@users.noreply.github.com>
2021-02-13 18:11:21 +0530

    update data_prep

--- data_prep.py
+++ data_prep.py
@@ -43,7 +43,7 @@ def data_prep():
 
     if acc_id!="":
         data=pd.DataFrame()
-        dat_type = st.radio("Select the type of Data",('Pre-Processed Data','Raw Data'))
+        dat_type = st.radio("Select the type of Data",('Pre-Processed Data','Raw Data(Test)'))
         if dat_type=='Pre-Processed Data':
             mode="preprocessed"
             prepro_data = st.file_uploader("Upload Preprocessed Data", type=["csv"])
